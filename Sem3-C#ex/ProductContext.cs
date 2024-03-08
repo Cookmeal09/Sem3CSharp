ed9e6bfa99906e1e62c793e9d60203d16b87274f
Cookmeal09 <Cookmeal09@gmail.com>
2024-03-08 10:37:14 +0700

    first commit

---
+++ Sem3-C#ex/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem3_C_ex
+{
+    public partial class ProductContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ database/Cake.sql
@@ -0,0 +1,12 @@
+create database dtcsharp
+go
+use dtcsharp
+go
+
+create table cake(
+	 Id int primary key,
+	 Image_name varchar(100),
+	 Size int,
+	 Price decimal,
+)
+
